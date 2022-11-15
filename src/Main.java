4da596733ea03ba5c35ab28760a3316f9bbef4bd
quirogaaaa <alvaroquiroga4045@gmail.com>
2022-11-15 17:30:48 +0100

    blabla

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,12 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Main extends JFrame{
-    JPanel panelIzquierdo;
-    JLabel imagen;
+    JPanel panelIzquierdo,panelInferior;
     JPanel panelTarjetas;
     CardLayout tarjetas;
-    JButton siguiente;
     JPanel[] panel = new JPanel[4];
     JEditorPane p1 = new JEditorPane();
+    BorderLayout miBorderLayout;
 
 
     public Main(){
@@ -18,21 +17,106 @@ public class Main extends JFrame{
 
 
         initPanelTarjetas();
+        initPaneluno();
+        initPaneldos();
         initPanelIzq();
+        initpanelInferior();
         initBoton();
         initPantalla();
     }
 
     private void initBoton(){
-        siguiente = new JButton("Siguiente");
+        JButton siguiente,anterior;
+        anterior = new JButton("anterior");
+        anterior.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(!panelTarjetas.getComponent(0).isShowing()){
+                    tarjetas.previous(panelTarjetas);
+                }
+            }
+        });
+        panelInferior.add(anterior);
+        siguiente = new JButton("siguiente");
         siguiente.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                tarjetas.next(panelTarjetas);
+                if(!panelTarjetas.getComponent(3).isShowing()){
+                    tarjetas.next(panelTarjetas);
+                }
             }
         });
-        panelIzquierdo.add(siguiente);
+        panelInferior.add(siguiente);
 
+    }
+
+    private void initPaneluno(){
+        p1.setContentType("text/html");
+        p1.setText(
+                "<h1><b>Formulario</h1></b>"+
+                        "<font size=8>"+
+                        "<b>Bienvenido al formulario oficial de Alvaro Quiroga<b>"+"<br>"+
+                        "<br>"+
+                        "<b>En este formulario se le pedira el datos personales y pais <b>"+
+                        "<b>Despues se le mostara el<br> resultado de este mismo formulario y se le descargara en una ruta especificada<b>"+
+                        "<br>"+"<br>"+
+                        "<b>Todos sus datos estan protegidos y puede confiar en nosotros <b>"+
+                        "</font>"
+        );
+        p1.setBackground(Color.decode("#edede9"));
+        panel[0].add(p1);
+    }
+    private void initPaneldos(){
+        JTextField nombre,correo;
+        JPasswordField contraseña;
+        JLabel labelNombre,labelCorreo,labelContraseña;
+        panel[1].setLayout(new GridBagLayout());
+
+        GridBagConstraints[] grid = new GridBagConstraints[6];
+        int y = 0;
+        int x = 0;
+        for (int i = 0; i < grid.length; i++) {
+            grid[i] = new GridBagConstraints();
+            grid[i].anchor = GridBagConstraints.WEST;
+            if(x<2) {
+
+                grid[i].gridx = x;
+                grid[i].gridy = y;
+
+            }
+            if(x==2) {
+                y++; x=0;
+                grid[i].gridx = x;
+                grid[i].gridy = y;
+            }
+            x++;
+        }
+
+        labelNombre = new JLabel("Nombre ");
+        labelCorreo = new JLabel("Correo ");
+        labelContraseña = new JLabel("Contraseña ");
+        labelNombre.setFont(new Font("MONOSPACED",Font.PLAIN,50));
+        labelCorreo.setFont(new Font("MONOSPACED",Font.PLAIN,50));
+        labelContraseña.setFont(new Font("MONOSPACED",Font.PLAIN,50));
+
+
+        nombre = new JTextField();
+        correo = new JTextField();
+        contraseña = new JPasswordField();
+        nombre.setPreferredSize(new Dimension(300,45));
+        correo.setPreferredSize(new Dimension(600,45));
+        contraseña.setPreferredSize(new Dimension(300,40));
+        nombre.setFont(new Font("MONOSPACED",Font.PLAIN,40));
+        correo.setFont(new Font("MONOSPACED",Font.PLAIN,40));
+        contraseña.setFont(new Font("MONOSPACED",Font.PLAIN,40));
+
+
+        panel[1].add(labelNombre,grid[0]);
+        panel[1].add(nombre,grid[1]);
+        panel[1].add(labelCorreo,grid[2]);
+        panel[1].add(correo,grid[3]);
+        panel[1].add(labelContraseña,grid[4]);
+        panel[1].add(contraseña,grid[5]);
     }
 
     private void initPanelTarjetas(){
@@ -45,23 +129,10 @@ public class Main extends JFrame{
             panelTarjetas.add(panel[i]);
         }
         add(panelTarjetas,BorderLayout.CENTER);
-        p1.setText(
-                "<h1><b>Formulario</h1></b>"
-
-
-
-
-
-        );
-        p1.setBackground(Color.decode("#edede9"));
-        panel[0].add(p1);
-        //asdfdsf
-
-
-
 
     }
     private void initPanelIzq(){
+        JLabel imagen;
         panelIzquierdo = new JPanel();
         panelIzquierdo.setPreferredSize(new Dimension(270, 800));
         panelIzquierdo.setBackground(Color.decode("#d6ccc2"));
@@ -72,10 +143,18 @@ public class Main extends JFrame{
         panelIzquierdo.add(imagen);
         add(panelIzquierdo,BorderLayout.WEST);
     }
+    private void initpanelInferior(){
+
+
+        panelInferior = new JPanel();
+        panelInferior.setLayout(new FlowLayout(FlowLayout.RIGHT,5,0));
+        panelInferior.setPreferredSize(new Dimension(1500, 30));
+        add(panelInferior,BorderLayout.SOUTH);
+    }
     private void initPantalla() {
 
         setTitle("Formulario"); //Título del JFrame
-        setBounds(500,100,900,800); //Dimensiones del JFrame
+        setBounds(250,100,1600,800); //Dimensiones del JFrame
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Cerrar proceso al cerrar ventana
         setVisible(true); //Mostrar JFrame
         getContentPane().setBackground(Color.decode("#d5bdaf"));
